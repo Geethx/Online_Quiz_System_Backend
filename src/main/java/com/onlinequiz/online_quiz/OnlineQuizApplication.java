7fa3ccd263f4e7596336a6c16b7de598c00723a4
Geethx <jeewangeethanga@gmail.com>
2026-04-26 10:38:36 +0530

    feat: initialize Spring Boot application with Asia/Colombo default timezone

--- src/main/java/com/onlinequiz/online_quiz/OnlineQuizApplication.java
+++ src/main/java/com/onlinequiz/online_quiz/OnlineQuizApplication.java
@@ -3,8 +3,17 @@ package com.onlinequiz.online_quiz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class OnlineQuizApplication {
+
+	@PostConstruct
+	public void init() {
+		// Set the default timezone to Sri Lanka
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Colombo"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineQuizApplication.class, args);
